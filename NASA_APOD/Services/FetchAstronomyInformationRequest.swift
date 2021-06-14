c6286203b7733e0b7a40972a201ce0a197dc1ee2
Balkrishna Singbal <balkrishnasingbal@gmail.com>
2021-06-14 21:01:10 +0530

    Updated request class.

--- NASA_APOD/Services/FetchAstronomyInformationRequest.swift
+++ NASA_APOD/Services/FetchAstronomyInformationRequest.swift
@@ -11,12 +11,18 @@ class FetchAstronomyInformationRequest {
   
   private static let ENDPOINT = "https://api.nasa.gov/planetary/apod?api_key=MPWAW9EG61juVkcQUR95pfDi2al7ofe2TrfL38Mk"
   
+  private var request: URLRequest
+  
+  init() {
+    request = URLRequest(url: URL(string: FetchAstronomyInformationRequest.ENDPOINT)!)
+    request.httpMethod = "GET"
+    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+  }
+  
   func execute(completion: @escaping (Result<AstronomyInformationResponse, Error>) -> ()) {
-    let url = URL(string: FetchAstronomyInformationRequest.ENDPOINT)!
     
-    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+    let task = URLSession.shared.dataTask(with: request) {(data, response, error) in
       
-//      print(String(data: data, encoding: .utf8)!)
       guard error == nil else {
         completion(.failure(error!))
         return
