9caa2b5bee42a4d3b2c346b8a69621b71f62fa24
Balkrishna Singbal <balkrishnasingbal@gmail.com>
2021-06-14 19:21:09 +0530

    Designed AstronomyViewController UI.

---
+++ NASA_APOD/AstronomyPictureOfTheDayViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  NASA_APOD
+//
+//  Created by Balkrishna Singbal on 14/06/21.
+//
+
+import UIKit
+
+class AstronomyPictureOfTheDayViewController: UIViewController {
+
+  // MARK:- IBOutlets
+  @IBOutlet weak var astronomyImageView: UIImageView!
+  @IBOutlet weak var titleLabel: UILabel!
+  @IBOutlet weak var explainationLabel: UILabel!
+  
+  // MARK:- Lifecycle methods
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
