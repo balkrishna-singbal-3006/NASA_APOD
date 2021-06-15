81428e4fea94124ff55ee79a4c4ac93389019695
Balkrishna Singbal <balkrishnasingbal@gmail.com>
2021-06-15 10:25:48 +0530

    Code refactoring - service layer.

--- NASA_APOD/Services/FetchAstronomyInformationRequest.swift
+++ NASA_APOD/Services/FetchAstronomyInformationRequest.swift
@@ -10,16 +10,22 @@ import Foundation
 class FetchAstronomyInformationRequest: GETRequestable {
   
   typealias ResponseType = AstronomyInformationResponse
-
-  var endpoint: String = "https://api.nasa.gov/planetary/apod?api_key=MPWAW9EG61juVkcQUR95pfDi2al7ofe2TrfL38Mk"
+  
+  var endpoint: String = "https://api.nasa.gov/planetary/apod"
   
   var request: URLRequest
+  
+  private let apiKey: String = "MPWAW9EG61juVkcQUR95pfDi2al7ofe2TrfL38Mk"
   
   /**
    Initializes the service to fetch astronomy information.
    */
   init() {
-    request = URLRequest(url: URL(string: endpoint)!)
+    
+    var urlComponents = URLComponents(string: endpoint)!
+    urlComponents.queryItems = [URLQueryItem(name: "api_key", value: apiKey)]
+    
+    request = URLRequest(url: urlComponents.url!)
     request.httpMethod = "GET"
     request.addValue("application/json", forHTTPHeaderField: "Content-Type")
   }
