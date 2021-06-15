e064af266776d612c5f3c2266d9711158608843e
Balkrishna Singbal <balkrishnasingbal@gmail.com>
2021-06-15 14:43:09 +0530

    Saving image to documents directory.

--- NASA_APOD/Extensions/UIImage+Extension.swift
+++ NASA_APOD/Extensions/UIImage+Extension.swift
@@ -11,16 +11,54 @@ extension UIImageView {
   
   func downloaded(from url: URL) {
     self.contentMode = .scaleAspectFit
-          URLSession.shared.dataTask(with: url) { data, response, error in
-              guard
-                  let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
-                  let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
-                  let data = data, error == nil,
-                  let image = UIImage(data: data)
-                  else { return }
-              DispatchQueue.main.async() { [weak self] in
-                  self?.image = image
-              }
-          }.resume()
+    URLSession.shared.dataTask(with: url) { data, response, error in
+      
+      guard error == nil else {
+        DispatchQueue.main.async {
+          self.image = self.getSavedImage(named: "astronomyPictureOfTheDay.png")
+        }
+        return
       }
+      
+      guard
+        let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+        let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+        let data = data,
+        let image = UIImage(data: data)
+      else {
+        print("Returning from guard...")
+        return }
+      
+      DispatchQueue.main.async() {
+        self.image = image
+        self.saveImage(image: image)
+      }
+    }.resume()
+  }
+  
+  @discardableResult
+  private func saveImage(image: UIImage) -> Bool {
+    guard let data = image.jpegData(compressionQuality: 1) ?? image.pngData() else {
+      return false
+    }
+    guard let directory = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) as NSURL else {
+      return false
+    }
+    do {
+      try data.write(to: directory.appendingPathComponent("astronomyPictureOfTheDay.png")!)
+      print("### Saved image to Documents Directory....")
+      return true
+    } catch {
+      print(error.localizedDescription)
+      return false
+    }
+  }
+  
+  private func getSavedImage(named: String) -> UIImage? {
+    if let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false) {
+      print("### Fetching image from Documents Directory....")
+      return UIImage(contentsOfFile: URL(fileURLWithPath: dir.absoluteString).appendingPathComponent(named).path)
+    }
+    return nil
+  }
 }
